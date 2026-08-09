da4f9c80bb94aee302844e916559f77b06db06cf
Alexander <alexandernapo.90@gmail.com>
2026-08-09 00:33:47 -0500

    icono transparente

--- vite.config.js
+++ vite.config.js
@@ -28,12 +28,6 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png',
           },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
         ],
       },
     }),
